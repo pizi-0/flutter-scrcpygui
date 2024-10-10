8f9a32135e152b7a15ca1f6dd8c331036eeea51a
pizi <hafizirahim92@gmail.com>
2024-10-11 07:34:31 +0800

    fix unable to start server for usb device if app is hidden on plug

--- lib/screens/main_screen/ms_desktop.dart
+++ lib/screens/main_screen/ms_desktop.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scrcpygui/providers/adb_provider.dart';
+import 'package:scrcpygui/providers/info_provider.dart';
+import 'package:scrcpygui/utils/adb_utils.dart';
 import 'package:scrcpygui/utils/tray_utils.dart';
 import 'package:scrcpygui/widgets/start_stop_button.dart';
 import 'package:tray_manager/tray_manager.dart';
@@ -23,6 +26,20 @@ class _DesktopMainScreenState extends ConsumerState<DesktopMainScreen>
   void initState() {
     TrayUtils.initTray(ref);
     trayManager.addListener(this);
+    ref.read(adbProvider.notifier).ref.listenSelf((prev, next) async {
+      if (prev!.length < next.length) {
+        final currentInfo = ref.read(infoProvider);
+
+        for (final d in next) {
+          if (currentInfo
+              .where((i) => i.device.serialNo == d.serialNo)
+              .isEmpty) {
+            final info = await AdbUtils.getScrcpyDetailsFor(d);
+            ref.read(infoProvider.notifier).addInfo(info);
+          }
+        }
+      }
+    });
     super.initState();
   }
 
