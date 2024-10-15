0a91ede257b26fe872cf69490f09f0b3eb6cced3
pizi <hafizirahim92@gmail.com>
2024-10-15 09:57:40 +0800

    color from hex to #rrggbb if only 3 provided

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -5,7 +5,20 @@ extension HexColor on Color {
   static Color fromHex(String hexString) {
     final buffer = StringBuffer();
     if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
+    if (hexString.length == 3 || hexString.length == 4) {
+      var newHex = '';
+      for (var c in hexString.split('')) {
+        final l = newHex.split('');
+        l.add('$c$c');
+
+        newHex = l.join();
+      }
+
+      hexString = newHex;
+
+      buffer.write('ff');
+    }
+    buffer.write(hexString.replaceAll('#', ''));
     return Color(int.parse(buffer.toString(), radix: 16));
   }
 
