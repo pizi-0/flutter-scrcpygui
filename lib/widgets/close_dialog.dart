720b121c00a0de80d7b2da366d22d369da62e826
pizi <hafizirahim92@gmail.com>
2024-10-17 08:56:43 +0800

    config close dialog tweak

--- lib/widgets/close_dialog.dart
+++ lib/widgets/close_dialog.dart
@@ -159,8 +159,6 @@ class _CloseDialogState extends ConsumerState<CloseDialog> {
                       ref.read(selectedConfigProvider.notifier).state =
                           currentConfig;
 
-                      print(currentConfig.configName);
-
                       final toSave = ref
                           .read(configsProvider)
                           .where((e) => !defaultConfigs.contains(e))
