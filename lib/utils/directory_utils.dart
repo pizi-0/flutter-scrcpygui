5ff99738fd643f80d220b09349cb9f57059ee872
pizi <hafizirahim92@gmail.com>
2025-01-08 07:50:36 +0800

    add dir utils to cleanup code

---
+++ lib/utils/directory_utils.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class DirectoryUtils {
+  static var sep = Platform.pathSeparator;
+
+  static Future<Directory> getExecDir() async {
+    final supportDir = await getApplicationSupportDirectory();
+
+    final dir = Directory('${supportDir.path}${sep}exec');
+
+    if (!dir.existsSync()) {
+      await dir.create();
+    }
+
+    return dir;
+  }
+
+  static Future<Directory> getScrcpyVersionDir(String version,
+      {bool createIfNot = false}) async {
+    final execDir = await getExecDir();
+    final dir = Directory('${execDir.path}$sep$version');
+
+    if (createIfNot && !dir.existsSync()) {
+      await dir.create();
+    }
+
+    return Directory('${execDir.path}$sep$version');
+  }
+
+  static Future<Directory> getDownloadDir() async {
+    final cacheDir = await getApplicationCacheDirectory();
+    final dir = Directory('${cacheDir.path}${sep}download');
+
+    if (!dir.existsSync()) {
+      await dir.create();
+    }
+
+    return dir;
+  }
+}
