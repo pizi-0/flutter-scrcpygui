a4e7155f9bb5cc625db40d1bda1b9b583181828e
pizi-0 <hafizirahim92@gmail.com>
2025-02-11 08:29:05 +0800

    object equality

--- lib/models/installed_scrcpy.dart
+++ lib/models/installed_scrcpy.dart
@@ -1,6 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class InstalledScrcpy {
   final String version;
   final String path;
 
   InstalledScrcpy({required this.version, required this.path});
+
+  @override
+  bool operator ==(covariant InstalledScrcpy other) {
+    if (identical(this, other)) return true;
+
+    return other.version == version && other.path == path;
+  }
+
+  @override
+  int get hashCode => version.hashCode ^ path.hashCode;
 }
