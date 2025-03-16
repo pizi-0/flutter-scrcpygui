64be49344a7e9fda132619037dc9b1ba83d7ec0c
pizi <hafizirahim92@gmail.com>
2025-03-16 16:25:51 +0800

    string extension

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -30,3 +30,39 @@ extension ListExtension on List {
     }
   }
 }
+
+extension ResolutionStringExtension on String {
+  /// Checks if the string is a valid resolution format (e.g., "1920x1080").
+  ///
+  /// Returns `true` if the string matches the resolution format, `false` otherwise.
+  bool get isValidResolution {
+    // Regular expression to match the resolution format:
+    // - ^: Matches the beginning of the string.
+    // - \d+: Matches one or more digits (for the width).
+    // - x: Matches the literal "x" character.
+    // - \d+: Matches one or more digits (for the height).
+    // - $: Matches the end of the string.
+    final resolutionRegex = RegExp(r'^\d+x\d+$');
+    return resolutionRegex.hasMatch(this);
+  }
+
+  /// Returns the width of the resolution if the string is a valid resolution format.
+  ///
+  /// Returns `null` if the string is not a valid resolution format.
+  int? get resolutionWidth {
+    if (!isValidResolution) {
+      return null;
+    }
+    return int.tryParse(this.split('x').first);
+  }
+
+  /// Returns the height of the resolution if the string is a valid resolution format.
+  ///
+  /// Returns `null` if the string is not a valid resolution format.
+  int? get resolutionHeight {
+    if (!isValidResolution) {
+      return null;
+    }
+    return int.tryParse(this.split('x').last);
+  }
+}
