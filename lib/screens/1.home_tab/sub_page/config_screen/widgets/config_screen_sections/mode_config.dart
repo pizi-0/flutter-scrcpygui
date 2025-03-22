6adeaa098215e39f9e27ee1d4c75dcee44b26cc7
pizi <hafizirahim92@gmail.com>
2025-03-22 12:19:33 +0800

    cleanup

--- lib/screens/1.home_tab/sub_page/config_screen/widgets/config_screen_sections/mode_config.dart
+++ lib/screens/1.home_tab/sub_page/config_screen/widgets/config_screen_sections/mode_config.dart
@@ -26,122 +26,200 @@ class _ModeConfigState extends ConsumerState<ModeConfig> {
 
   @override
   Widget build(BuildContext context) {
-    final selectedConfig = ref.watch(configScreenConfig)!;
-    final showInfo = ref.watch(configScreenShowInfo);
+    final isRecording =
+        ref.watch(configScreenConfig.select((val) => val!.isRecording));
 
     return PgSectionCard(
       label: el.modeSection.title,
       children: [
-        ConfigDropdownEnum<MainMode>(
-          items: MainMode.values,
-          title: el.modeSection.mainMode.label,
-          subtitle: !selectedConfig.isRecording
-              ? el.modeSection.mainMode.info.default$
-              : el.modeSection.mainMode.info.alt,
-          showinfo: showInfo,
-          initialValue:
-              (selectedConfig.isRecording ? MainMode.record : MainMode.mirror),
-          onSelected: (value) {
-            bool isRecording = value == MainMode.record;
-
-            ref
-                .read(configScreenConfig.notifier)
-                .setModeConfig(isRecording: isRecording);
-
-            modeLabel = value!.value;
-            setState(() {});
-          },
-        ),
-        if (selectedConfig.isRecording) const Divider(),
-        if (selectedConfig.isRecording)
-          FadeIn(
-            child: ConfigCustom(
-              dimTitle: false,
-              title: el.modeSection.saveFolder.label,
-              subtitle: el.modeSection.saveFolder.info,
-              showinfo: showInfo,
-              child: SecondaryButton(
-                trailing: const Icon(Icons.folder),
-                onPressed: () async {
-                  final res = await FilePicker.platform.getDirectoryPath();
-
-                  if (res != null) {
-                    ref
-                        .read(configScreenConfig.notifier)
-                        .setModeConfig(savePath: res);
-                  }
-                },
-                child: Text(
-                  (selectedConfig.savePath ?? '').split(sep).last,
-                ),
-              ),
-            ),
-          ),
+        MainModeFlag(),
+        if (isRecording) const Divider(),
+        if (isRecording) FadeIn(child: SaveFolderFlag()),
         const Divider(),
-        ConfigDropdownEnum<ScrcpyMode>(
-          items: ScrcpyMode.values,
-          title: modeLabel,
-          subtitle: selectedConfig.scrcpyMode == ScrcpyMode.both
-              ? el.modeSection.scrcpyMode.info.default$
-              : el.modeSection.scrcpyMode.info
-                  .alt(command: selectedConfig.scrcpyMode.command.trim()),
-          showinfo: showInfo,
-          initialValue: selectedConfig.scrcpyMode,
-          onSelected: (value) {
-            ref
-                .read(configScreenConfig.notifier)
-                .setModeConfig(scrcpyMode: value);
-
-            if (value == ScrcpyMode.audioOnly) {
-              final audioFormat = _audioFormat(selectedConfig);
-
-              ref
-                  .read(configScreenConfig.notifier)
-                  .setVideoConfig(setToDefault: true);
-
-              ref.read(configScreenConfig.notifier).setAudioConfig(
-                    audioFormat: audioFormat,
-                    audioBitrate: selectedConfig.audioOptions.audioBitrate,
-                    audioCodec: selectedConfig.audioOptions.audioCodec,
-                    audioEncoder: selectedConfig.audioOptions.audioEncoder,
-                    audioSource: selectedConfig.audioOptions.audioSource,
-                    duplicateAudio: selectedConfig.audioOptions.duplicateAudio,
-                  );
-            }
-
-            if (value == ScrcpyMode.videoOnly) {
-              ref
-                  .read(configScreenConfig.notifier)
-                  .setAudioConfig(setToDefault: true);
-
-              ref.read(configScreenConfig.notifier).setVideoConfig(
-                    displayId: selectedConfig.videoOptions.displayId,
-                    maxFPS: selectedConfig.videoOptions.maxFPS,
-                    resolutionScale:
-                        selectedConfig.videoOptions.resolutionScale,
-                    videoBitrate: selectedConfig.videoOptions.videoBitrate,
-                    videoCodec: selectedConfig.videoOptions.videoCodec,
-                    videoEncoder: selectedConfig.videoOptions.videoEncoder,
-                    videoFormat: selectedConfig.videoOptions.videoFormat,
-                  );
-            }
-          },
-        )
+        ScrcpyModeFlag()
       ],
     );
   }
+}
 
-  _audioFormat(ScrcpyConfig selectedConfig) {
-    switch (selectedConfig.audioOptions.audioCodec) {
-      case 'aac':
-        return AudioFormat.aac;
-      case 'flac':
-        return AudioFormat.flac;
-      case 'raw':
-        return AudioFormat.wav;
+_audioFormat(ScrcpyConfig selectedConfig) {
+  switch (selectedConfig.audioOptions.audioCodec) {
+    case 'aac':
+      return AudioFormat.aac;
+    case 'flac':
+      return AudioFormat.flac;
+    case 'raw':
+      return AudioFormat.wav;
 
-      default:
-        return AudioFormat.opus;
-    }
+    default:
+      return AudioFormat.opus;
   }
+}
+
+class MainModeFlag extends ConsumerWidget {
+  const MainModeFlag({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isRecording =
+        ref.watch(configScreenConfig.select((val) => val!.isRecording));
+    final showInfo = ref.watch(configScreenShowInfo);
+
+    return ConfigCustom(
+      title: el.modeSection.mainMode.label,
+      subtitle: !isRecording
+          ? el.modeSection.mainMode.info.default$
+          : el.modeSection.mainMode.info.alt,
+      showinfo: showInfo,
+      dimTitle: false,
+      child: Select(
+        value: isRecording ? mainModeDD(context)[1] : mainModeDD(context)[0],
+        filled: true,
+        popup: SelectPopup(
+          items: SelectItemList(
+            children: mainModeDD(context)
+                .map(
+                  (mode) => SelectItemButton(value: mode, child: Text(mode.$2)),
+                )
+                .toList(),
+          ),
+        ).call,
+        onChanged: (value) =>
+            ref.read(configScreenConfig.notifier).setModeConfig(
+                  isRecording: value!.$1 == MainMode.record,
+                ),
+        itemBuilder: (context, value) => Text(value.$2),
+      ),
+    );
+  }
+}
+
+class SaveFolderFlag extends ConsumerWidget {
+  const SaveFolderFlag({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final showInfo = ref.watch(configScreenShowInfo);
+    final savePath =
+        ref.watch(configScreenConfig.select((val) => val!.savePath));
+
+    return ConfigCustom(
+      dimTitle: false,
+      title: el.modeSection.saveFolder.label,
+      subtitle: el.modeSection.saveFolder.info,
+      showinfo: showInfo,
+      child: SecondaryButton(
+        trailing: const Icon(Icons.folder),
+        onPressed: () async {
+          final res = await FilePicker.platform.getDirectoryPath();
+
+          if (res != null) {
+            ref.read(configScreenConfig.notifier).setModeConfig(savePath: res);
+          }
+        },
+        child: Text(
+          (savePath ?? '').split(sep).last,
+        ),
+      ),
+    );
+  }
+}
+
+class ScrcpyModeFlag extends ConsumerWidget {
+  const ScrcpyModeFlag({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final showInfo = ref.watch(configScreenShowInfo);
+    final isRecording =
+        ref.watch(configScreenConfig.select((val) => val!.isRecording));
+
+    final scrcpyMode =
+        ref.watch(configScreenConfig.select((val) => val!.scrcpyMode));
+
+    final audioOptions =
+        ref.watch(configScreenConfig.select((val) => val!.audioOptions));
+
+    final videoOptions =
+        ref.watch(configScreenConfig.select((val) => val!.videoOptions));
+
+    return ConfigCustom(
+      dimTitle: false,
+      title: isRecording
+          ? el.modeSection.mainMode.record
+          : el.modeSection.mainMode.mirror,
+      subtitle: scrcpyMode == ScrcpyMode.both
+          ? el.modeSection.scrcpyMode.info.default$
+          : el.modeSection.scrcpyMode.info
+              .alt(command: scrcpyMode.command.trim()),
+      showinfo: showInfo,
+      child: Select(
+        value:
+            scrcpyModeDD(context).firstWhere((mode) => mode.$1 == scrcpyMode),
+        onChanged: (value) {
+          ref
+              .read(configScreenConfig.notifier)
+              .setModeConfig(scrcpyMode: value!.$1);
+
+          if (value.$1 == ScrcpyMode.audioOnly) {
+            final audioFormat = _audioFormat(ref.read(selectedConfigProvider)!);
+
+            ref
+                .read(configScreenConfig.notifier)
+                .setVideoConfig(setToDefault: true);
+
+            ref.read(configScreenConfig.notifier).setAudioConfig(
+                  audioFormat: audioFormat,
+                  audioBitrate: audioOptions.audioBitrate,
+                  audioCodec: audioOptions.audioCodec,
+                  audioEncoder: audioOptions.audioEncoder,
+                  audioSource: audioOptions.audioSource,
+                  duplicateAudio: audioOptions.duplicateAudio,
+                );
+          }
+
+          if (value.$1 == ScrcpyMode.videoOnly) {
+            ref
+                .read(configScreenConfig.notifier)
+                .setAudioConfig(setToDefault: true);
+
+            ref.read(configScreenConfig.notifier).setVideoConfig(
+                  displayId: videoOptions.displayId,
+                  maxFPS: videoOptions.maxFPS,
+                  resolutionScale: videoOptions.resolutionScale,
+                  videoBitrate: videoOptions.videoBitrate,
+                  videoCodec: videoOptions.videoCodec,
+                  videoEncoder: videoOptions.videoEncoder,
+                  videoFormat: videoOptions.videoFormat,
+                );
+          }
+        },
+        itemBuilder: (context, value) => Text(value.$2),
+        filled: true,
+        popup: SelectPopup(
+          items: SelectItemList(
+            children: scrcpyModeDD(context)
+                .map((mode) =>
+                    SelectItemButton(value: mode, child: Text(mode.$2)))
+                .toList(),
+          ),
+        ).call,
+      ),
+    );
+  }
+}
+
+List<(MainMode, String)> mainModeDD(BuildContext context) {
+  return [
+    (MainMode.mirror, el.modeSection.mainMode.mirror),
+    (MainMode.record, el.modeSection.mainMode.record),
+  ];
+}
+
+List<(ScrcpyMode, String)> scrcpyModeDD(BuildContext context) {
+  return [
+    (ScrcpyMode.both, el.modeSection.scrcpyMode.both),
+    (ScrcpyMode.audioOnly, el.modeSection.scrcpyMode.audioOnly),
+    (ScrcpyMode.videoOnly, el.modeSection.scrcpyMode.videoOnly),
+  ];
 }
