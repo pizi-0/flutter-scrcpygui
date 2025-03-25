03e83ad9674dab510f10d764d897f9d793f578af
pizi <hafizirahim92@gmail.com>
2025-03-25 18:09:12 +0800

    add more info

--- lib/screens/1.home_tab/widgets/bottom_bar/widgets/config_detail_dialog.dart
+++ lib/screens/1.home_tab/widgets/bottom_bar/widgets/config_detail_dialog.dart
@@ -2,6 +2,7 @@ import 'package:awesome_extensions/awesome_extensions.dart'
     show StringExtension;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:localization/localization.dart';
 import 'package:scrcpygui/utils/const.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
@@ -18,118 +19,154 @@ class ConfigDetailDialog extends ConsumerStatefulWidget {
 }
 
 class _ConfigDetailDialogState extends ConsumerState<ConfigDetailDialog> {
+  ScrollController scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: appWidth),
-      child: AlertDialog(
-        title: const Text('Config info'),
-        content: SingleChildScrollView(
-          child: Column(
-            spacing: 6,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Name: ${widget.config.configName}'),
-              Text('Mode: ${widget.config.scrcpyMode.value.capitalizeFirst}'),
-              Text('Record: ${widget.config.isRecording}'),
-              if (widget.config.isRecording)
-                Text('Save folder: ${widget.config.savePath}'),
-              if (widget.config.scrcpyMode != ScrcpyMode.audioOnly)
+    final size = MediaQuery.sizeOf(context);
+
+    final videoOptions = widget.config.videoOptions;
+    final virtualDisplayOptions = videoOptions.virtualDisplayOptions;
+    final audioOptions = widget.config.audioOptions;
+    final appOptions = widget.config.appOptions;
+    final deviceOptions = widget.config.deviceOptions;
+    final windowOptions = widget.config.windowOptions;
+
+    return AlertDialog(
+      title: const Text('Config info'),
+      content: ConstrainedBox(
+        constraints:
+            BoxConstraints(maxWidth: appWidth, maxHeight: size.height - 161),
+        child: Scrollbar(
+          controller: scrollController,
+          thumbVisibility: true,
+          child: SingleChildScrollView(
+            controller: scrollController,
+            scrollDirection: Axis.vertical,
+            child: Column(
+              spacing: 6,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Name: ${widget.config.configName}'),
+                Text('Mode: ${widget.config.scrcpyMode.value.capitalizeFirst}'),
+                Text('Record: ${widget.config.isRecording}'),
+                if (widget.config.isRecording)
+                  Text('Save folder: ${widget.config.savePath}'),
+                if (widget.config.scrcpyMode != ScrcpyMode.audioOnly)
+                  Column(
+                    spacing: 2,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Divider(),
+                      const Text('Video options:'),
+                      if (widget.config.isRecording)
+                        Text('  - format: ${videoOptions.videoFormat.value}'),
+                      Text('  - codec: ${videoOptions.videoCodec}'),
+                      Text('  - encoder: ${videoOptions.videoEncoder}'),
+                      Text('  - bitrate: ${videoOptions.videoBitrate}M'),
+                      Text(
+                          '  - resolution scale: ${videoOptions.resolutionScale}'),
+                      Text('  - display id: ${videoOptions.displayId}'),
+                    ],
+                  ),
+                if (videoOptions.displayId == 'new')
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 2,
+                    children: [
+                      Divider(),
+                      Text('Virtual display options: '),
+                      Text(
+                          ' - resolution: ${virtualDisplayOptions.resolution}'),
+                      Text(' - dpi: ${virtualDisplayOptions.dpi}'),
+                      if (virtualDisplayOptions.disableDecorations)
+                        Text(
+                            '  - disable decorations: ${virtualDisplayOptions.disableDecorations}'),
+                      if (virtualDisplayOptions.preseveContent)
+                        Text(
+                            '  - preserve content: ${virtualDisplayOptions.preseveContent}')
+                    ],
+                  ),
+                if (widget.config.scrcpyMode != ScrcpyMode.videoOnly)
+                  Column(
+                    spacing: 2,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Divider(),
+                      const Text('Audio options:'),
+                      if (widget.config.isRecording)
+                        Text('  - format: ${audioOptions.audioFormat.value}'),
+                      Text('  - codec: ${audioOptions.audioCodec}'),
+                      Text(
+                          '  - encoder: ${audioOptions.audioCodec == 'opus' ? 'default' : audioOptions.audioEncoder}'),
+                      Text('  - bitrate: ${audioOptions.audioBitrate}K'),
+                    ],
+                  ),
+                if (appOptions?.selectedApp != null)
+                  Column(
+                    spacing: 2,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Divider(),
+                      const Text('App options:'),
+                      Text('  - app: ${appOptions!.selectedApp!.name}'),
+                      Text('  - force close app: ${appOptions.forceClose}')
+                    ],
+                  ),
                 Column(
                   spacing: 2,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Divider(),
-                    const Text('Video options:'),
-                    if (widget.config.isRecording)
-                      Text(
-                          '  - format: ${widget.config.videoOptions.videoFormat.value}'),
-                    Text('  - codec: ${widget.config.videoOptions.videoCodec}'),
-                    Text(
-                        '  - encoder: ${widget.config.videoOptions.videoEncoder}'),
-                    Text(
-                        '  - bitrate: ${widget.config.videoOptions.videoBitrate}M'),
-                    Text(
-                        '  - resolution scale: ${widget.config.videoOptions.resolutionScale}'),
-                    Text(
-                        '  - display id: ${widget.config.videoOptions.displayId}'),
+                    const Text('Device options:'),
+                    if (deviceOptions.stayAwake) const Text('  - stay awake'),
+                    if (deviceOptions.showTouches)
+                      const Text('  - show touches'),
+                    if (deviceOptions.turnOffDisplay)
+                      const Text('  - turn off display on start'),
+                    if (deviceOptions.offScreenOnClose)
+                      const Text('  - turn off display on on exit'),
+                    if (deviceOptions.noScreensaver)
+                      const Text('  - disable screensaver (HOST)'),
                   ],
                 ),
-              if (widget.config.scrcpyMode != ScrcpyMode.videoOnly)
                 Column(
                   spacing: 2,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Divider(),
-                    const Text('Audio options:'),
-                    if (widget.config.isRecording)
-                      Text(
-                          '  - format: ${widget.config.audioOptions.audioFormat.value}'),
-                    Text('  - codec: ${widget.config.audioOptions.audioCodec}'),
-                    Text(
-                        '  - encoder: ${widget.config.audioOptions.audioCodec == 'opus' ? 'default' : widget.config.audioOptions.audioEncoder}'),
-                    Text(
-                        '  - bitrate: ${widget.config.audioOptions.audioBitrate}K'),
+                    const Text('Window options:'),
+                    if (windowOptions.noWindow) const Text('  - hide window'),
+                    if (windowOptions.noBorder) const Text('  - borderless'),
+                    if (windowOptions.alwaysOntop)
+                      const Text('  - always on top'),
+                    if (windowOptions.timeLimit != 0)
+                      Text('  - time limit: ${windowOptions.timeLimit}s'),
                   ],
                 ),
-              Column(
-                spacing: 2,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Divider(),
-                  const Text('Device options:'),
-                  if (widget.config.deviceOptions.stayAwake)
-                    const Text('  - stay awake'),
-                  if (widget.config.deviceOptions.showTouches)
-                    const Text('  - show touches'),
-                  if (widget.config.deviceOptions.turnOffDisplay)
-                    const Text('  - turn off display on start'),
-                  if (widget.config.deviceOptions.offScreenOnClose)
-                    const Text('  - turn off display on on exit'),
-                  if (widget.config.deviceOptions.noScreensaver)
-                    const Text('  - disable screensaver (HOST)'),
-                ],
-              ),
-              Column(
-                spacing: 2,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Divider(),
-                  const Text('Window options:'),
-                  if (widget.config.windowOptions.noWindow)
-                    const Text('  - hide window'),
-                  if (widget.config.windowOptions.noBorder)
-                    const Text('  - borderless'),
-                  if (widget.config.windowOptions.alwaysOntop)
-                    const Text('  - always on top'),
-                  if (widget.config.windowOptions.timeLimit != 0)
-                    Text(
-                        '  - time limit: ${widget.config.windowOptions.timeLimit}s'),
-                ],
-              ),
-              Column(
-                spacing: 2,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Divider(),
-                  const Text('Additional flags:'),
-                  ...widget.config.additionalFlags
-                      .split('--')
-                      .where((e) => e.isNotEmpty)
-                      .map((e) => Text('  - --${e.trim()}'))
-                ],
-              ),
-            ],
+                Column(
+                  spacing: 2,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Divider(),
+                    const Text('Additional flags:'),
+                    ...widget.config.additionalFlags
+                        .split('--')
+                        .where((e) => e.isNotEmpty)
+                        .map((e) => Text('  --${e.trim()}'))
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-        actions: [
-          SecondaryButton(
-            child: const Text('Close'),
-            onPressed: () => context.pop(),
-          )
-        ],
       ),
+      actions: [
+        SecondaryButton(
+          child: Text(el.buttonLabelLoc.close),
+          onPressed: () => context.pop(),
+        )
+      ],
     );
   }
 }
