7d50c9ef937c9cbdbe0550497aa0f8bb5cf2af78
pizi <hafizirahim92@gmail.com>
2025-03-30 15:28:29 +0800

    add new version section

--- lib/screens/about_tab/about_tab.dart
+++ lib/screens/about_tab/about_tab.dart
@@ -1,25 +1,126 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:localization/localization.dart';
 import 'package:scrcpygui/providers/settings_provider.dart';
 import 'package:scrcpygui/providers/version_provider.dart';
+import 'package:scrcpygui/utils/app_utils.dart';
+import 'package:scrcpygui/utils/extension.dart';
+import 'package:scrcpygui/widgets/config_tiles.dart';
 import 'package:scrcpygui/widgets/custom_ui/pg_scaffold.dart';
 import 'package:scrcpygui/widgets/custom_ui/pg_section_card.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class AboutTab extends ConsumerWidget {
+class AboutTab extends ConsumerStatefulWidget {
   static const String route = '/about';
   const AboutTab({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<AboutTab> createState() => _AboutTabState();
+}
+
+class _AboutTabState extends ConsumerState<AboutTab> {
+  String? latest;
+  bool loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  _getLatest() async {
+    loading = true;
+    setState(() {});
+
+    final current = ref.read(appVersionProvider);
+
+    try {
+      latest = await AppUtils.getLatestAppVersion();
+
+      if (latest!.parseVersionToInt()! > current.parseVersionToInt()!) {
+        showToast(
+          context: context,
+          showDuration: 1.5.seconds,
+          location: ToastLocation.bottomCenter,
+          builder: (context, overlay) => SurfaceCard(
+            child: Basic(
+              title: Text(el.scrcpyManagerLoc.updater.label),
+              trailing: Icon(Icons.new_releases_outlined, color: Colors.lime),
+            ),
+          ),
+        );
+      } else {
+        showToast(
+          context: context,
+          showDuration: 1.5.seconds,
+          location: ToastLocation.bottomCenter,
+          builder: (context, overlay) => SurfaceCard(
+            child: Basic(
+              title: Text(el.scrcpyManagerLoc.infoPopup.noUpdate),
+              trailing: Icon(Icons.info_outline_rounded),
+            ),
+          ),
+        );
+      }
+    } on Exception catch (_) {
+      loading = false;
+      setState(() {});
+
+      showToast(
+        context: context,
+        showDuration: 1.5.seconds,
+        location: ToastLocation.bottomCenter,
+        builder: (context, overlay) => SurfaceCard(
+          child: Basic(
+            title: Text(el.scrcpyManagerLoc.infoPopup.error),
+            trailing: Icon(Icons.error_outline_rounded, color: Colors.red),
+          ),
+        ),
+      );
+    }
+
+    loading = false;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     ref.watch(settingsProvider.select((sett) => sett.behaviour.languageCode));
     final appversion = ref.watch(appVersionProvider);
+
     return PgScaffold(
       title: el.aboutLoc.title,
+      appBarTrailing: [
+        IconButton.ghost(
+          icon: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: loading
+                ? const CircularProgressIndicator(size: 20)
+                : const Icon(Icons.refresh),
+          ),
+          onPressed: _getLatest,
+        ),
+      ],
       children: [
+        if (latest != null &&
+            latest!.parseVersionToInt()! > appversion.parseVersionToInt()!)
+          PgSectionCard(
+            label: el.scrcpyManagerLoc.updater.newVersion,
+            children: [
+              ConfigCustom(
+                dimTitle: false,
+                title: el.buttonLabelLoc.update,
+                child: OutlineButton(
+                  trailing: Icon(Icons.open_in_new),
+                  child: Text('Github'),
+                  onPressed: () => launchUrl(Uri.parse('$guiGit/releases')),
+                ),
+              ),
+            ],
+          ),
         PgSectionCard(
           label: 'Scrcpy GUI',
           children: [
