18cdc45d7a7f0c4055b8054b14246746fddfff95
pizi <hafizirahim92@gmail.com>
2025-04-02 17:57:12 +0800

    change back button behaviour to show save button

--- lib/screens/1.home_tab/sub_page/config_screen/config_screen.dart
+++ lib/screens/1.home_tab/sub_page/config_screen/config_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'package:animate_do/animate_do.dart';
+import 'package:awesome_extensions/awesome_extensions.dart' show NumExtension;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:localization/localization.dart';
@@ -9,7 +11,6 @@ import 'package:scrcpygui/models/scrcpy_related/scrcpy_config.dart';
 
 import 'package:scrcpygui/models/scrcpy_related/scrcpy_enum.dart';
 import 'package:scrcpygui/providers/config_provider.dart';
-import 'package:scrcpygui/providers/scrcpy_provider.dart';
 import 'package:scrcpygui/providers/version_provider.dart';
 import 'package:scrcpygui/screens/1.home_tab/sub_page/config_screen/widgets/config_screen_sections/app_config.dart';
 import 'package:scrcpygui/utils/adb_utils.dart';
@@ -25,7 +26,6 @@ import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 import '../../../../providers/adb_provider.dart';
 import '../../../../utils/const.dart';
-import '../../../../utils/scrcpy_utils.dart';
 import 'widgets/close_dialog.dart';
 import 'widgets/config_screen_sections/config_rename.dart';
 
@@ -84,39 +84,62 @@ class _ConfigScreenState extends ConsumerState<ConfigScreen> {
     super.dispose();
   }
 
-  _handleOnClose() async {
-    final allConfigs = ref.read(configsProvider);
-    final selectedConfig = ref.read(configScreenConfig);
-    final testInstance = ref.read(testInstanceProvider);
-    final selectedDevice = ref.read(selectedDeviceProvider);
+  // _handleOnClose() async {
+  //   final allConfigs = ref.read(configsProvider);
+  //   final selectedConfig = ref.read(configScreenConfig);
+  //   final testInstance = ref.read(testInstanceProvider);
+  //   final selectedDevice = ref.read(selectedDeviceProvider);
 
-    if (testInstance != null) {
-      await ScrcpyUtils.killServer(testInstance);
-      ref.read(scrcpyInstanceProvider.notifier).removeInstance(testInstance);
-      ref.read(testInstanceProvider.notifier).state = null;
-    }
+  //   if (testInstance != null) {
+  //     await ScrcpyUtils.killServer(testInstance);
+  //     ref.read(scrcpyInstanceProvider.notifier).removeInstance(testInstance);
+  //     ref.read(testInstanceProvider.notifier).state = null;
+  //   }
 
-    if (allConfigs.contains(selectedConfig) ||
-        selectedConfig == newConfig ||
-        selectedDevice == null) {
-      context.pop();
-    } else {
-      showDialog(
-        barrierColor: Colors.black.withValues(alpha: 0.9),
-        context: context,
-        builder: (context) {
-          return Center(child: ConfigScreenCloseDialog(oldConfig: oldConfig));
-        },
-      ).then((v) {
-        if ((v as bool?) ?? false) {
+  //   if (allConfigs.contains(selectedConfig) ||
+  //       selectedConfig == newConfig ||
+  //       selectedDevice == null) {
+  //     context.pop();
+  //   } else {
+  //     showDialog(
+  //       barrierColor: Colors.black.withValues(alpha: 0.9),
+  //       context: context,
+  //       builder: (context) {
+  //         return Center(child: ConfigScreenCloseDialog(oldConfig: oldConfig));
+  //       },
+  //     ).then((v) {
+  //       if ((v as bool?) ?? false) {
+  //         context.pop();
+  //       }
+  //     });
+  //   }
+  // }
+
+  _saveConfig() async {
+    showDialog(
+      barrierColor: Colors.black.withValues(alpha: 0.9),
+      context: context,
+      builder: (context) {
+        return Center(child: ConfigScreenCloseDialog(oldConfig: oldConfig));
+      },
+    ).then((v) {
+      final res = (v as CloseDialogResult?) ?? CloseDialogResult.cancel;
+
+      switch (res) {
+        case CloseDialogResult.save:
+          setState(() => oldConfig = ref.read(configScreenConfig)!);
           context.pop();
-        }
-      });
-    }
+        case CloseDialogResult.discard:
+          context.pop();
+        default:
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     final allConfigs = ref.watch(configsProvider);
     final selectedConfig = ref.watch(configScreenConfig)!;
     final showInfo = ref.watch(configScreenShowInfo);
@@ -125,11 +148,54 @@ class _ConfigScreenState extends ConsumerState<ConfigScreen> {
     final isEditing =
         allConfigs.where((c) => c.id == selectedConfig.id).isNotEmpty;
 
-    final hasChanges = oldConfig != selectedConfig;
+    final hasChanges = oldConfig != selectedConfig &&
+        defaultConfigs
+            .where((conf) => selectedConfig.isSimilarTo(conf))
+            .isEmpty;
+
+    final similar = allConfigs.where((conf) =>
+        !defaultConfigs.contains(conf) &&
+        selectedConfig.isSimilarTo(conf) &&
+        conf.id != selectedConfig.id);
 
     return PopScope(
       canPop: false,
       child: PgScaffold(
+        footers: [
+          if (similar.isNotEmpty)
+            FadeInUp(
+              duration: 200.milliseconds,
+              child: Container(
+                color: theme.colorScheme.destructive,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Basic(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 8,
+                      children: [
+                        Text(el.configScreenLoc
+                            .similarExist(count: similar.length.toString())),
+                        HoverCard(
+                            hoverBuilder: (context) => SurfaceCard(
+                                    child: Basic(
+                                  content: Column(
+                                    spacing: 4,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: similar
+                                        .map((conf) => Text(conf.configName))
+                                        .toList(),
+                                  ),
+                                )),
+                            child: Icon(Icons.info_outline_rounded).iconSmall())
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
         showLoading: dev.info == null,
         appBarTrailing: [
           Checkbox(
@@ -140,9 +206,15 @@ class _ConfigScreenState extends ConsumerState<ConfigScreen> {
                 .update((state) => !state),
           ),
         ],
-        onBack: selectedDevice == null
-            ? () => context.pop()
-            : () => _handleOnClose(),
+        leading: [
+          IconButton.ghost(
+            onPressed:
+                hasChanges && similar.isEmpty ? _saveConfig : context.pop,
+            icon: hasChanges && similar.isEmpty
+                ? Icon(Icons.save_rounded)
+                : Icon(Icons.arrow_back_rounded),
+          ),
+        ],
         title: hasChanges
             ? '${el.configScreenLoc.title}*'
             : el.configScreenLoc.title,
