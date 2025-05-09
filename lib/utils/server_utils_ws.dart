3eaa1b5be9f52a72cfc7df260956fc0e0a043880
pizi <hafizirahim92@gmail.com>
2025-05-09 22:08:46 +0800

    server socket

--- lib/utils/server_utils_ws.dart
+++ lib/utils/server_utils_ws.dart
@@ -58,65 +58,18 @@ class ServerUtilsWs {
 
     socket.listen(
       (data) async {
-        if (!authenticatedSockets.contains(socket)) {
-          try {
-            logger.i('Authenticating client: $clientAddress');
-            // final expectedKey = ref.read(companionServerProvider).secret;
-            // final receivedKey = XOR().xorDecode(utf8.decode(data));
+        final authd = await _authenticate(ref, socket, data);
 
-            final expectedKey = 'pass';
-            final receivedKey = 'pass';
-
-            if (receivedKey != expectedKey) {
-              logger.w('Authentication failed: $clientAddress');
-              socket.write('Invalid API Key');
-              await socket.close();
-              return;
-            } else {
-              logger.i('Authentication successful: $clientAddress');
-              authenticatedSockets.add(socket);
-              final devices = ref.read(adbProvider);
-              socket.write(ServerPayload(
-                      type: ServerPayloadType.devices,
-                      payload: jsonEncode(
-                          devices.map((d) => d.toPayload()).toList()))
-                  .toJson());
-
-              final configs = ref.read(configsProvider);
-              socket.write(
-                ServerPayload(
-                        type: ServerPayloadType.configs,
-                        payload: jsonEncode(
-                            configs.map((c) => c.toPayload()).toList()))
-                    .toJson(),
-              );
-
-              final instances = ref.read(scrcpyInstanceProvider);
-              socket.write(ServerPayload(
-                      type: ServerPayloadType.runnings,
-                      payload: jsonEncode(
-                          instances.map((i) => i.toPayload()).toList()))
-                  .toJson());
-
-              final appConfigPairs = ref.read(appConfigPairProvider);
-              socket.write(ServerPayload(
-                      type: ServerPayloadType.pairs,
-                      payload: jsonEncode(
-                          appConfigPairs.map((p) => p.toPayload()).toList()))
-                  .toJson());
-            }
-          } on Exception catch (e) {
-            logger.w('Authentication failed: $clientAddress');
-            socket.write(e);
-            await socket.close();
-            return;
-          }
+        if (!authd) {
+          return;
         }
 
-        print(utf8.decode(data));
-
-        // final payload = ClientPayload.fromJson(utf8.decode(data));
-        // await _handleRequest(ref, socket: socket, payload: payload);
+        try {
+          final payload = ClientPayload.fromJson(utf8.decode(data));
+          await _handleRequest(ref, socket: socket, payload: payload);
+        } on Exception catch (e) {
+          logger.e('Error handling request: $e');
+        }
       },
       onDone: () {
         authenticatedSockets.removeWhere((sock) =>
@@ -136,6 +89,70 @@ class ServerUtilsWs {
     await payload.toAction(ref);
   }
 
+  Future<bool> _authenticate(
+      WidgetRef ref, Socket socket, List<int> data) async {
+    final clientAddress = socket.remoteAddress.address;
+    if (!authenticatedSockets.contains(socket)) {
+      try {
+        logger.i('Authenticating client: $clientAddress');
+        final expectedKey = ref.read(companionServerProvider).secret;
+        final receivedKey = XOR().xorDecode(utf8.decode(data));
+
+        // final expectedKey = 'pass';
+        // final receivedKey = 'pass';
+
+        if (receivedKey != expectedKey) {
+          logger.w('Authentication failed: $clientAddress');
+          socket.write('Invalid API Key');
+          await socket.close();
+          return false;
+        } else {
+          logger.i('Authentication successful: $clientAddress');
+          authenticatedSockets.add(socket);
+
+          // Send initial data
+          final devices = ref.read(adbProvider);
+          final configs = ref.read(configsProvider);
+          final instances = ref.read(scrcpyInstanceProvider);
+          final appConfigPairs = ref.read(appConfigPairProvider);
+
+          final initialData = '${jsonEncode([
+                ServerPayload(
+                    type: ServerPayloadType.devices,
+                    payload:
+                        jsonEncode(devices.map((d) => d.toPayload()).toList())),
+                ServerPayload(
+                    type: ServerPayloadType.configs,
+                    payload:
+                        jsonEncode(configs.map((c) => c.toPayload()).toList())),
+                ServerPayload(
+                    type: ServerPayloadType.pairs,
+                    payload: jsonEncode(
+                        appConfigPairs.map((p) => p.toPayload()).toList())),
+                ServerPayload(
+                    type: ServerPayloadType.runnings,
+                    payload: jsonEncode(
+                        instances.map((r) => r.toPayload()).toList())),
+              ])}\n';
+
+          socket.write(
+            ServerPayload(
+                    type: ServerPayloadType.initialData, payload: initialData)
+                .toJson(),
+          );
+        }
+        return false;
+      } on Exception catch (e) {
+        logger.w('Authentication failed: $clientAddress');
+        socket.write(e);
+        await socket.close();
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+
   startServer(WidgetRef ref) async {
     try {
       await _bindServer();
@@ -143,9 +160,76 @@ class ServerUtilsWs {
       serverSocket!.listen(
         (socket) => _onData(ref, socket),
         onDone: () => serverSocket!.close(),
-        onError: (error) => logger.e('Server error', error: error.toString()),
+        onError: (error) {
+          serverSocket!.close();
+          logger.e('Server error', error: error.toString());
+        },
         cancelOnError: true,
       );
+
+      ref.listenManual(
+        adbProvider,
+        (previous, next) {
+          for (final sock in authenticatedSockets) {
+            sock.write(
+              '${ServerPayload(
+                type: ServerPayloadType.devices,
+                payload: jsonEncode(next.map((e) => e.toPayload()).toList()),
+              ).toJson()}\n',
+            );
+          }
+        },
+        fireImmediately: false,
+      );
+
+      ref.listenManual(
+        configsProvider,
+        (previous, next) {
+          for (final sock in authenticatedSockets) {
+            sock.write(
+              '${ServerPayload(
+                type: ServerPayloadType.configs,
+                payload: jsonEncode(next.map((e) => e.toPayload()).toList()),
+              ).toJson()}\n',
+            );
+          }
+        },
+        fireImmediately: false,
+      );
+
+      ref.listenManual(
+        appConfigPairProvider,
+        (previous, next) {
+          for (final sock in authenticatedSockets) {
+            sock.write(
+              '${ServerPayload(
+                type: ServerPayloadType.pairs,
+                payload: jsonEncode(next.map((e) => e.toPayload()).toList()),
+              ).toJson()}\n',
+            );
+          }
+        },
+        fireImmediately: false,
+      );
+
+      ref.listenManual(
+        scrcpyInstanceProvider,
+        (previous, next) async {
+          for (final sock in authenticatedSockets) {
+            sock.write(
+              '${ServerPayload(
+                type: ServerPayloadType.runnings,
+                payload: jsonEncode(next.map((e) => e.toPayload()).toList()),
+              ).toJson()}\n',
+            );
+          }
+        },
+        fireImmediately: false,
+        onError: (error, stackTrace) => logger.e(error),
+      );
+
+      _isServerRunning = true;
+      logger.i('Server started');
     } catch (e) {
       logger.e('Failed to start server', error: e.toString());
       await serverSocket?.close();
@@ -162,6 +246,11 @@ class ServerUtilsWs {
       _isServerRunning = false;
       _endpoint = null;
       _port = null;
+
+      for (final sock in authenticatedSockets) {
+        sock.close();
+      }
+
       authenticatedSockets.clear();
     } catch (e) {
       logger.e('Failed to stop server', error: e.toString());
