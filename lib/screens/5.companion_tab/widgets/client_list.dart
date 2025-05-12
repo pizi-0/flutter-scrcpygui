240e1d9eb7bf92d25f3e582989e6dd6a9ca84a3a
pizi <hafizirahim92@gmail.com>
2025-05-12 20:46:52 +0800

    cleanup

---
+++ lib/screens/5.companion_tab/widgets/client_list.dart
@@ -0,0 +1,164 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:awesome_extensions/awesome_extensions.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scrcpygui/models/companion_server/authenticated_client.dart';
+import 'package:scrcpygui/providers/companion_server_state_provider.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
+
+import '../../../db/db.dart';
+import '../../../providers/server_settings_provider.dart';
+import '../../../widgets/custom_ui/pg_list_tile.dart';
+import '../../../widgets/custom_ui/pg_section_card.dart';
+
+class ClientList extends ConsumerStatefulWidget {
+  const ClientList({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ClientListState();
+}
+
+class _ClientListState extends ConsumerState<ClientList> {
+  bool showBlocked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final companionSettings = ref.watch(companionServerProvider);
+    final serverState = ref.watch(companionServerStateProvider);
+    final clients = serverState.clients;
+    final blocked = companionSettings.blocklist;
+
+    return PgSectionCard(
+        label: 'Clients (${clients.length})',
+        labelTrail: Toggle(
+          style: ButtonStyle.ghost(density: ButtonDensity.dense),
+          value: showBlocked,
+          child: showBlocked
+              ? Row(
+                  spacing: 4,
+                  children: [
+                    Icon(Icons.close_rounded).iconSmall(),
+                    Text('Hide blocked (${blocked.length})')
+                  ],
+                )
+              : Row(
+                  spacing: 4,
+                  children: [
+                    Icon(Icons.block_rounded).iconSmall(),
+                    Text('Show blocked (${blocked.length})')
+                  ],
+                ),
+          onChanged: (value) => setState(() {
+            showBlocked = value;
+          }),
+        ),
+        children: showBlocked
+            ? [
+                if (companionSettings.blocklist.isNotEmpty)
+                  ...companionSettings.blocklist.mapIndexed(
+                    (idx, blocked) => FadeIn(
+                      duration: 100.milliseconds,
+                      child: Column(
+                        spacing: 8,
+                        children: [
+                          Row(
+                            spacing: 8,
+                            children: [
+                              Expanded(
+                                child: PgListTile(
+                                  title:
+                                      '${blocked.deviceName} / ${blocked.deviceModel}',
+                                  subtitle: blocked.clientAddress,
+                                  showSubtitle: true,
+                                  showSubtitleLeading: false,
+                                ),
+                              ),
+                              Tooltip(
+                                tooltip:
+                                    TooltipContainer(child: Text('Unblock'))
+                                        .call,
+                                child: IconButton.ghost(
+                                  icon: Icon(Icons.delete_rounded),
+                                  onPressed: () async {
+                                    ref
+                                        .read(companionServerProvider.notifier)
+                                        .removeFromBlocklist(blocked);
+
+                                    await Db.saveCompanionServerSettings(
+                                        ref.read(companionServerProvider));
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          if (idx != companionSettings.blocklist.length - 1)
+                            Divider()
+                        ],
+                      ),
+                    ),
+                  )
+                else
+                  FadeIn(
+                      duration: 100.milliseconds,
+                      child: Center(
+                          child: Text('No blocked clients').textSmall.muted))
+              ]
+            : [
+                if (clients.isNotEmpty)
+                  ...clients.mapIndexed(
+                    (idx, authd) => FadeIn(
+                      duration: 100.milliseconds,
+                      child: Column(
+                        spacing: 8,
+                        children: [
+                          Row(
+                            spacing: 8,
+                            children: [
+                              Expanded(
+                                child: PgListTile(
+                                  title:
+                                      '${authd.authPayload.deviceName} / ${authd.authPayload.deviceModel}',
+                                  subtitle: authd.socket.remoteAddress.address,
+                                  showSubtitle: true,
+                                  showSubtitleLeading: false,
+                                ),
+                              ),
+                              Tooltip(
+                                tooltip:
+                                    TooltipContainer(child: Text('Block')).call,
+                                child: IconButton.ghost(
+                                  icon: Icon(Icons.block),
+                                  onPressed: () async {
+                                    await authd.socket.close();
+                                    ref
+                                        .read(companionServerProvider.notifier)
+                                        .addToBlocklist(
+                                          BlockedClient(
+                                            clientAddress: authd.clientAddress,
+                                            deviceName:
+                                                authd.authPayload.deviceName,
+                                            deviceModel:
+                                                authd.authPayload.deviceModel,
+                                          ),
+                                        );
+
+                                    await Db.saveCompanionServerSettings(
+                                        ref.read(companionServerProvider));
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          if (idx != clients.length - 1) Divider()
+                        ],
+                      ),
+                    ),
+                  )
+                else
+                  FadeIn(
+                      duration: 100.milliseconds,
+                      child: Center(
+                          child: Text('No clients connected').textSmall.muted))
+              ]);
+  }
+}
---
+++ lib/screens/5.companion_tab/widgets/server_settings.dart
@@ -0,0 +1,290 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:awesome_extensions/awesome_extensions.dart';
+import 'package:encrypt_decrypt_plus/encrypt_decrypt/xor.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:localization/localization.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:scrcpygui/utils/server_utils_ws.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../db/db.dart';
+import '../../../providers/companion_server_state_provider.dart';
+import '../../../providers/server_settings_provider.dart';
+import '../../../utils/const.dart';
+import '../../../widgets/config_tiles.dart';
+import '../../../widgets/custom_ui/pg_section_card.dart';
+
+class ServerSettings extends ConsumerStatefulWidget {
+  const ServerSettings({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ServerSettingsState();
+}
+
+class _ServerSettingsState extends ConsumerState<ServerSettings> {
+  bool obscurePass = true;
+  bool showBlocked = false;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController portController = TextEditingController();
+  TextEditingController secretController = TextEditingController();
+  final serverUtils = ServerUtilsWs();
+
+  @override
+  void initState() {
+    super.initState();
+    nameController.text = ref.read(companionServerProvider).name;
+    nameController.addListener(() {
+      if (nameController.text.isEmpty) {
+        ref.read(companionServerProvider.notifier).setServerName('Scrcpy GUI');
+        return;
+      }
+
+      ref
+          .read(companionServerProvider.notifier)
+          .setServerName(nameController.text);
+    });
+
+    portController.text = ref.read(companionServerProvider).port;
+    portController.addListener(() {
+      if (portController.text.isEmpty) {
+        ref.read(companionServerProvider.notifier).setPort('8080');
+        return;
+      }
+
+      ref.read(companionServerProvider.notifier).setPort(portController.text);
+    });
+
+    secretController.text = ref.read(companionServerProvider).secret;
+    secretController.addListener(() {
+      if (secretController.text.isEmpty) {
+        ref
+            .read(companionServerProvider.notifier)
+            .setSecret('scrcpygui-is-okay');
+        return;
+      }
+
+      ref
+          .read(companionServerProvider.notifier)
+          .setSecret(secretController.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    portController.dispose();
+    secretController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final serverState = ref.watch(companionServerStateProvider);
+    final companionSettings = ref.watch(companionServerProvider);
+
+    final isServerRunning = serverState.isRunning;
+
+    return PgSectionCard(
+      label: el.companionLoc.server.label,
+      labelTrail: Button(
+          style: ButtonStyle.ghost(),
+          leading: isServerRunning
+              ? Icon(Icons.stop_rounded, color: Colors.red)
+              : Icon(Icons.play_arrow_rounded, color: Colors.green),
+          onPressed: () => _toggleServer(isServerRunning),
+          child: isServerRunning
+              ? Text(el.buttonLabelLoc.stop)
+              : Text(el.configLoc.start)),
+      children: [
+        ConfigCustom(
+          title: el.companionLoc.server.status,
+          dimTitle: false,
+          childExpand: false,
+          child: Text(
+                  isServerRunning ? el.statusLoc.running : el.statusLoc.stopped)
+              .textColor(isServerRunning ? Colors.green : Colors.red)
+              .textSmall,
+        ),
+        Divider(),
+        ConfigCustom(
+          title: el.companionLoc.server.name.label,
+          dimTitle: false,
+          child: TextField(
+            enabled: !isServerRunning,
+            controller: nameController,
+            filled: !isServerRunning,
+            placeholder: Text(el.companionLoc.server.name.info),
+          ),
+        ),
+        Divider(),
+        ConfigCustom(
+          title: el.companionLoc.server.port.label,
+          dimTitle: false,
+          child: TextField(
+            enabled: !isServerRunning,
+            controller: portController,
+            filled: !isServerRunning,
+            placeholder: Text(el.companionLoc.server.port.info),
+          ),
+        ),
+        Divider(),
+        ConfigCustom(
+          title: el.companionLoc.server.secret.label,
+          dimTitle: false,
+          child: TextField(
+            enabled: !isServerRunning,
+            controller: secretController,
+            obscureText: obscurePass,
+            filled: !isServerRunning,
+            features: [
+              InputFeature.trailing(IconButton.ghost(
+                density: ButtonDensity.iconDense,
+                icon: Icon(obscurePass
+                        ? Icons.visibility_off_rounded
+                        : Icons.visibility_rounded)
+                    .iconXSmall(),
+                onPressed: () {
+                  setState(() {
+                    obscurePass = !obscurePass;
+                  });
+                },
+              ))
+            ],
+          ),
+        ),
+        Divider(),
+        ConfigCustom(
+          title: el.companionLoc.server.autoStart.label,
+          childExpand: false,
+          onPressed: _toggleAutoStart,
+          child: Checkbox(
+            state: companionSettings.startOnLaunch
+                ? CheckboxState.checked
+                : CheckboxState.unchecked,
+            onChanged: (v) => _toggleAutoStart(),
+          ),
+        ),
+        if (isServerRunning) Divider(),
+        if (isServerRunning)
+          Row(
+            spacing: 10,
+            children: [
+              SizedBox.square(
+                dimension: 150,
+                child: QrImageView(
+                  data: XOR().xorEncode(companionSettings.toQrJson()),
+                  backgroundColor: Colors.white,
+                  padding: EdgeInsets.all(4),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  spacing: 8,
+                  children: [
+                    Text(el.companionLoc.qr)
+                        .textAlignment(TextAlign.center)
+                        .textSmall
+                        .muted,
+                    Tooltip(
+                      tooltip: TooltipContainer(
+                              child: Text(
+                                  'https://github.com/pizi-0/flutter_scrcpygui_companion'))
+                          .call,
+                      child: OutlineButton(
+                        leading: Icon(BootstrapIcons.github).iconSmall(),
+                        onPressed: () {
+                          launchUrl(Uri.parse(
+                              'https://github.com/pizi-0/flutter_scrcpygui_companion'));
+                        },
+                        child: Text('Github'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+      ],
+    );
+  }
+
+  _toggleServer(bool isServerRunning) async {
+    final companionSettingsNotifier =
+        ref.read(companionServerProvider.notifier);
+
+    obscurePass = true;
+
+    if (isServerRunning) {
+      await serverUtils.stopServer(ref);
+    } else {
+      final agree = (await showDialog<bool>(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text(el.serverDisclaimerLoc.title),
+              content: ConstrainedBox(
+                constraints:
+                    BoxConstraints(maxWidth: appWidth, minWidth: appWidth),
+                child: Text(
+                  el.serverDisclaimerLoc.contents,
+                ),
+              ),
+              actions: [
+                Button.primary(
+                  child: Text(el.buttonLabelLoc.serverAgree),
+                  onPressed: () => Navigator.of(context).pop(true),
+                ),
+                Button.destructive(
+                  child: Text(el.buttonLabelLoc.cancel),
+                  onPressed: () => Navigator.of(context).pop(false),
+                ),
+              ],
+            ),
+          )) ??
+          false;
+
+      if (!agree) return;
+
+      try {
+        await serverUtils.startServer(ref);
+        final port = ref.read(companionServerStateProvider).port;
+
+        companionSettingsNotifier.setPort(port);
+
+        portController.text = port;
+
+        companionSettingsNotifier.setEndpoint(
+            serverUtils.serverSocket?.address.address ?? '0.0.0.0');
+
+        await Db.saveCompanionServerSettings(ref.read(companionServerProvider));
+      } on Exception catch (e) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+              title: Text(el.statusLoc.error),
+              content: ConstrainedBox(
+                constraints:
+                    BoxConstraints(maxWidth: appWidth, minWidth: appWidth),
+                child: Text(
+                    'Failed to start companion server.\n\n${e.toString()}'),
+              ),
+              actions: [
+                Button.secondary(
+                  onPressed: () => context.pop(),
+                  child: Text(el.buttonLabelLoc.close),
+                )
+              ]),
+        );
+      }
+    }
+  }
+
+  _toggleAutoStart() async {
+    final companionSettingsNotifier =
+        ref.read(companionServerProvider.notifier);
+
+    companionSettingsNotifier.setStartOnLaunch();
+    await Db.saveCompanionServerSettings(ref.read(companionServerProvider));
+  }
+}
