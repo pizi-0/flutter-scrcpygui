a1571c87d9370b7804c3c0f1d0f8cb3905a9e5bd
pizi-0 <hafizirahim92@gmail.com>
2025-05-29 23:24:00 +0800

    tweak

--- lib/screens/1.home_tab/widgets/home/widgets/pinned_app_display.dart
+++ lib/screens/1.home_tab/widgets/home/widgets/pinned_app_display.dart
@@ -1,16 +1,9 @@
-import 'package:awesome_extensions/awesome_extensions.dart';
-import 'package:collection/collection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:scrcpygui/db/db.dart';
-import 'package:scrcpygui/models/scrcpy_related/scrcpy_config.dart';
+import 'package:scrcpygui/screens/1.home_tab/sub_page/device_control/widgets/app_grid.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 import '../../../../../models/adb_devices.dart';
-import '../../../../../models/app_config_pair.dart';
-import '../../../../../providers/adb_provider.dart';
 import '../../../../../providers/app_config_pair_provider.dart';
-import '../../../../../providers/config_provider.dart';
-import '../../../../../utils/scrcpy_utils.dart';
 
 class PinnedAppDisplay extends ConsumerWidget {
   final AdbDevices device;
@@ -19,80 +12,20 @@ class PinnedAppDisplay extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appConfigPairs =
-        ref.watch(appConfigPairProvider.select((pair) => pair.where((p) => p.deviceId == device.id)));
+        ref.watch(appConfigPairProvider.select((pair) => pair.where((p) => p.deviceId == device.id))).toList();
+
+    appConfigPairs.sort((a, b) => a.app.name.toLowerCase().compareTo(b.app.name.toLowerCase()));
 
     return GridView.builder(
       shrinkWrap: true,
       itemCount: appConfigPairs.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisSpacing: 2, mainAxisSpacing: 2, crossAxisCount: 3, childAspectRatio: 16 / 4.2),
+          crossAxisSpacing: 8, mainAxisSpacing: 8, crossAxisCount: 3, childAspectRatio: 150 / 40),
       itemBuilder: (context, index) {
         final pair = appConfigPairs.toList()[index];
 
-        return PinnedAppChip(pair: pair);
+        return AppGridTile(ref: ref, device: device, app: pair.app);
       },
     );
-  }
-}
-
-class PinnedAppChip extends ConsumerStatefulWidget {
-  final AppConfigPair pair;
-  const PinnedAppChip({super.key, required this.pair});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _PinnedAppChipState();
-}
-
-class _PinnedAppChipState extends ConsumerState<PinnedAppChip> {
-  bool loading = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final allConfigs = ref.watch(configsProvider);
-    final connected = ref.watch(adbProvider);
-
-    final config = allConfigs.firstWhereOrNull((c) => c.id == widget.pair.config.id);
-    final device = connected.firstWhereOrNull((conn) => conn.id == widget.pair.deviceId);
-
-    final noConfig = config == null;
-
-    return Tooltip(
-      tooltip: TooltipContainer(
-              child: noConfig
-                  ? Text('Missing config: ${widget.pair.config.configName}')
-                  : Text('On: ${config.configName}'))
-          .call,
-      child: SizedBox(
-        width: 119.1,
-        child: Chip(
-          style: noConfig ? ButtonStyle.destructive() : ButtonStyle.secondary(),
-          onPressed: loading || noConfig ? null : () => _start(config, device!),
-          trailing: ChipButton(
-            onPressed: () {
-              ref.read(appConfigPairProvider.notifier).removePair(widget.pair);
-              Db.saveAppConfigPairs(ref.read(appConfigPairProvider));
-            },
-            child: loading ? CircularProgressIndicator(size: 15) : Icon(Icons.close),
-          ),
-          child: OverflowMarquee(duration: 3.seconds, delayDuration: 0.5.seconds, child: Text(widget.pair.app.name)),
-        ),
-      ),
-    );
-  }
-
-  _start(ScrcpyConfig config, AdbDevices device) async {
-    loading = true;
-    setState(() {});
-    await ScrcpyUtils.newInstance(
-      ref,
-      selectedConfig: config.copyWith(appOptions: (config.appOptions).copyWith(selectedApp: widget.pair.app)),
-      selectedDevice: device,
-      customInstanceName: '${widget.pair.app.name} (${config.configName})',
-    );
-
-    if (mounted) {
-      loading = false;
-      setState(() {});
-    }
   }
 }
