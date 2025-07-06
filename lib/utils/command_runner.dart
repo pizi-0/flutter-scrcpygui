e57eb619842c8d28767471a7b22157134fd43ce4
pizi-0 <hafizirahim92@gmail.com>
2025-07-06 08:05:33 +0800

    fixes #22, refactor

--- lib/utils/command_runner.dart
+++ lib/utils/command_runner.dart
@@ -1,85 +1,58 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:path/path.dart' as p;
 
 import '../models/adb_devices.dart';
 
 class CommandRunner {
+  static String _getExecutablePath(String workDir, String executableName) {
+    if (Platform.isWindows) {
+      return p.join(workDir, '$executableName.exe');
+    } else if (Platform.isLinux || Platform.isMacOS) {
+      return p.join(workDir, executableName);
+    } else {
+      throw UnsupportedError(
+          'Unsupported platform: ${Platform.operatingSystem}');
+    }
+  }
+
   static Future<ProcessResult> runAdbCommand(String workDir,
       {List<String> args = const []}) async {
-    try {
-      if (Platform.isWindows) {
-        return await Process.run('$workDir\\adb.exe', args,
-            workingDirectory: workDir);
-      } else if (Platform.isLinux || Platform.isMacOS) {
-        return await Process.run('$workDir/adb', args,
-            workingDirectory: workDir);
-      } else {
-        throw Exception('Unsupported platform');
-      }
-    } on Exception catch (_) {
-      rethrow;
-    }
+    final adbPath = _getExecutablePath(workDir, 'adb');
+
+    return Process.run(adbPath, args,
+        stdoutEncoding: Utf8Codec(),
+        stderrEncoding: Utf8Codec(),
+        workingDirectory: workDir);
   }
 
   static Future<Process> startAdbCommand(String workDir,
       {List<String> args = const []}) async {
-    try {
-      if (Platform.isWindows) {
-        final res = await Process.start('$workDir\\adb.exe', args,
-            workingDirectory: workDir);
-        return res;
-      } else if (Platform.isLinux || Platform.isMacOS) {
-        final res = await Process.start('$workDir/adb', args,
-            workingDirectory: workDir);
-
-        return res;
-      } else {
-        throw Exception('Unsupported platform');
-      }
-    } on Exception catch (_) {
-      rethrow;
-    }
+    final adbPath = _getExecutablePath(workDir, 'adb');
+    return Process.start(adbPath, args, workingDirectory: workDir);
   }
 
   static Future<ProcessResult> runAdbShellCommand(
       String workDir, AdbDevices device,
       {List<String> args = const []}) async {
-    return await CommandRunner.runAdbCommand(workDir,
+    return CommandRunner.runAdbCommand(workDir,
         args: ['-s', device.id, 'shell', ...args]);
   }
 
   static Future<ProcessResult> runScrcpyCommand(
       String workDir, AdbDevices device,
       {List<String> args = const []}) async {
-    if (Platform.isWindows) {
-      final res = await Process.run(
-          '$workDir\\scrcpy.exe', ['-s', device.id, ...args],
-          workingDirectory: workDir);
-      return res;
-    } else if (Platform.isLinux || Platform.isMacOS) {
-      final res = await Process.run(
-          '$workDir/scrcpy', ['-s', device.id, ...args],
-          workingDirectory: workDir);
-      return res;
-    } else {
-      throw Exception('Unsupported platform');
-    }
+    final scrcpyPath = _getExecutablePath(workDir, 'scrcpy');
+    return Process.run(scrcpyPath, ['-s', device.id, ...args],
+        stdoutEncoding: Utf8Codec(),
+        stderrEncoding: Utf8Codec(),
+        workingDirectory: workDir);
   }
 
   static Future<Process> startScrcpyCommand(String workDir, AdbDevices device,
       {List<String> args = const []}) async {
-    if (Platform.isWindows) {
-      final res = await Process.start(
-          '$workDir\\scrcpy.exe', ['-s', device.id, ...args],
-          workingDirectory: workDir);
-      return res;
-    } else if (Platform.isLinux || Platform.isMacOS) {
-      final res = await Process.start(
-          '$workDir/scrcpy', ['-s', device.id, ...args],
-          workingDirectory: workDir);
-
-      return res;
-    } else {
-      throw Exception('Unsupported platform');
-    }
+    final scrcpyPath = _getExecutablePath(workDir, 'scrcpy');
+    return Process.start(scrcpyPath, ['-s', device.id, ...args],
+        workingDirectory: workDir);
   }
 }
