96bc70d5a6c8da353fb77c16d693f3fdcd239f33
pizi <hafizirahim92@gmail.com>
2025-07-07 14:06:59 +0800

    companion tab fix layout

--- lib/screens/5.companion_tab/companion_tab.dart
+++ lib/screens/5.companion_tab/companion_tab.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localization/localization.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:scrcpygui/providers/settings_provider.dart';
 import 'package:scrcpygui/utils/server_utils_ws.dart';
+import 'package:scrcpygui/widgets/custom_ui/pg_column.dart';
 import 'package:scrcpygui/widgets/custom_ui/pg_scaffold.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
@@ -25,11 +28,55 @@ class _CompanionTabState extends ConsumerState<CompanionTab> {
   Widget build(BuildContext context) {
     ref.watch(settingsProvider.select((s) => s.behaviour.languageCode));
 
-    return PgScaffold(
-      title: el.companionLoc.title,
+    return PgScaffoldCustom(
+      title: Text(el.companionLoc.title).bold().underline,
+      scaffoldBody: ResponsiveBuilder(
+        builder: (context, sizeInfo) {
+          return AnimatedSwitcher(
+            duration: 200.milliseconds,
+            child: sizeInfo.isMobile || sizeInfo.isTablet
+                ? CompanionTabSmall()
+                : CompanionTabBig(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CompanionTabSmall extends StatelessWidget {
+  const CompanionTabSmall({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ServerSettings(),
+          ClientList(),
+        ],
+      ),
+    );
+  }
+}
+
+class CompanionTabBig extends StatelessWidget {
+  const CompanionTabBig({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 8,
       children: [
-        ServerSettings(),
-        ClientList(),
+        LeftColumn(
+          child: ServerSettings(
+            expandContent: true,
+          ),
+        ),
+        RightColumn(
+            child: ClientList(
+          expandContent: true,
+        )),
       ],
     );
   }
