834fc8db1b7f52e745fd940e581059b4aeba324d
pizi <hafizirahim92@gmail.com>
2025-07-07 14:43:45 +0800

    add flex

--- lib/widgets/custom_ui/pg_column.dart
+++ lib/widgets/custom_ui/pg_column.dart
@@ -3,33 +3,53 @@ import 'package:flutter/material.dart';
 class LeftColumn extends StatelessWidget {
   final Widget child;
   final Alignment alignment;
+  final int flex;
+  final bool shouldAlign;
 
   const LeftColumn(
-      {super.key, required this.child, this.alignment = Alignment.topRight});
+      {super.key,
+      required this.child,
+      this.alignment = Alignment.topRight,
+      this.shouldAlign = true,
+      this.flex = 1});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Align(
-      alignment: alignment,
-      child: child,
-    ));
+      flex: flex,
+      child: shouldAlign
+          ? Align(
+              alignment: alignment,
+              child: child,
+            )
+          : child,
+    );
   }
 }
 
 class RightColumn extends StatelessWidget {
   final Widget child;
   final Alignment alignment;
+  final bool shouldAlign;
+  final int flex;
 
   const RightColumn(
-      {super.key, required this.child, this.alignment = Alignment.topLeft});
+      {super.key,
+      required this.child,
+      this.alignment = Alignment.topLeft,
+      this.shouldAlign = true,
+      this.flex = 1});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Align(
-      alignment: alignment,
-      child: child,
-    ));
+      flex: flex,
+      child: shouldAlign
+          ? Align(
+              alignment: alignment,
+              child: child,
+            )
+          : child,
+    );
   }
 }
