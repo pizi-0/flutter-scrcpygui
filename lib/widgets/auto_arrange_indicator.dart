baafd819e9b010d5578f8df96c119d16acba171e
pizi-0 <hafizirahim92@gmail.com>
2025-08-03 22:47:14 +0800

    tweak auto arrange selector

--- lib/widgets/auto_arrange_indicator.dart
+++ lib/widgets/auto_arrange_indicator.dart
@@ -121,111 +121,187 @@ class AutoArrangeOriginSelector extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final autoArrangeOrigin =
-        ref.watch(settingsProvider).behaviour.autoArrangeOrigin;
-
-    int? selectedIndex;
-    switch (autoArrangeOrigin) {
-      case AutoArrangeOrigin.topLeft:
-        selectedIndex = 0;
-        break;
-      case AutoArrangeOrigin.topRight:
-        selectedIndex = 2;
-        break;
-      case AutoArrangeOrigin.bottomLeft:
-        selectedIndex = 6;
-        break;
-      case AutoArrangeOrigin.bottomRight:
-        selectedIndex = 8;
-        break;
-      case AutoArrangeOrigin.centerLeft:
-        selectedIndex = 3;
-        break;
-      case AutoArrangeOrigin.centerRight:
-        selectedIndex = 5;
-        break;
-      case AutoArrangeOrigin.off:
-        selectedIndex = null;
-        break;
-    }
-
     return SizedBox(
-      width: 116,
+      width: 150,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         spacing: 8,
         children: [
-          Switch(
-            leading: OverflowMarquee(child: Text('Arrange').textSmall),
-            value: autoArrangeOrigin != AutoArrangeOrigin.off,
-            onChanged: (value) async {
-              if (autoArrangeOrigin == AutoArrangeOrigin.off) {
-                ref.read(settingsProvider.notifier).changeAutoArrangeOrigin(
-                      AutoArrangeOrigin.topLeft,
-                    );
-              } else {
-                ref.read(settingsProvider.notifier).changeAutoArrangeOrigin(
-                      AutoArrangeOrigin.off,
-                    );
-              }
-              await Db.saveAppSettings(ref.read(settingsProvider));
-            },
-          ),
+          Text('Alignment').textSmall,
           Divider(),
-          SizedBox.square(
-            dimension: 100,
-            child: GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
+          Column(
+            spacing: 4,
+            children: [
+              AnimatedSize(
+                duration: 200.milliseconds,
+                child: Row(
+                  spacing: 4,
+                  children: [
+                    OriginBox(origin: AutoArrangeOrigin.topLeft),
+                    OriginBox(origin: AutoArrangeOrigin.topRight),
+                  ],
+                ),
               ),
-              itemCount: 9,
-              itemBuilder: (context, index) {
-                final isSelected = index == selectedIndex;
-                return Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Button(
-                    style: isSelected
-                        ? ButtonStyle.primary()
-                        : ButtonStyle.outline(),
-                    onPressed: _disabledIndices.contains(index)
-                        ? null
-                        : () async {
-                            ref
-                                .read(settingsProvider.notifier)
-                                .changeAutoArrangeOrigin(index == 0
-                                    ? AutoArrangeOrigin.topLeft
-                                    : index == 2
-                                        ? AutoArrangeOrigin.topRight
-                                        : index == 3
-                                            ? AutoArrangeOrigin.centerLeft
-                                            : index == 5
-                                                ? AutoArrangeOrigin.centerRight
-                                                : index == 6
-                                                    ? AutoArrangeOrigin
-                                                        .bottomLeft
-                                                    : index == 8
-                                                        ? AutoArrangeOrigin
-                                                            .bottomRight
-                                                        : AutoArrangeOrigin
-                                                            .off);
-                            await Db.saveAppSettings(
-                                ref.read(settingsProvider));
-                          },
-                    child: SizedBox.shrink(),
-                  ),
-                );
-              },
-            ),
+              AnimatedSize(
+                duration: 200.milliseconds,
+                child: Row(
+                  spacing: 4,
+                  children: [
+                    OriginBox(origin: AutoArrangeOrigin.centerLeft),
+                    OriginBox(origin: AutoArrangeOrigin.off),
+                    OriginBox(origin: AutoArrangeOrigin.centerRight),
+                  ],
+                ),
+              ),
+              AnimatedSize(
+                duration: 200.milliseconds,
+                child: Row(
+                  spacing: 4,
+                  children: [
+                    OriginBox(origin: AutoArrangeOrigin.bottomLeft),
+                    OriginBox(origin: AutoArrangeOrigin.bottomRight),
+                  ],
+                ),
+              ),
+            ],
           ),
+          // SizedBox.square(
+          //   dimension: 100,
+          //   child: GridView.builder(
+          //     shrinkWrap: true,
+          //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //       crossAxisCount: 3,
+          //     ),
+          //     itemCount: 9,
+          //     itemBuilder: (context, index) {
+          //       final isSelected = index == selectedIndex;
+          //       return Padding(
+          //         padding: const EdgeInsets.all(2.0),
+          //         child: Button(
+          //           style: isSelected
+          //               ? ButtonStyle.primary()
+          //               : ButtonStyle.outline(),
+          //           onPressed: _disabledIndices.contains(index)
+          //               ? null
+          //               : () async {
+          //                   ref
+          //                       .read(settingsProvider.notifier)
+          //                       .changeAutoArrangeOrigin(index == 0
+          //                           ? AutoArrangeOrigin.topLeft
+          //                           : index == 2
+          //                               ? AutoArrangeOrigin.topRight
+          //                               : index == 3
+          //                                   ? AutoArrangeOrigin.centerLeft
+          //                                   : index == 5
+          //                                       ? AutoArrangeOrigin.centerRight
+          //                                       : index == 6
+          //                                           ? AutoArrangeOrigin
+          //                                               .bottomLeft
+          //                                           : index == 8
+          //                                               ? AutoArrangeOrigin
+          //                                                   .bottomRight
+          //                                               : AutoArrangeOrigin
+          //                                                   .off);
+          //                   await Db.saveAppSettings(
+          //                       ref.read(settingsProvider));
+          //                 },
+          //           child: SizedBox.shrink(),
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
+          SizedBox(),
         ],
       ),
     );
   }
 }
 
-List<int> _disabledIndices = [
-  1, // Top center
-  4, // Center
-  7, // Bottom center
-];
+class OriginBox extends ConsumerStatefulWidget {
+  final AutoArrangeOrigin origin;
+
+  const OriginBox({super.key, required this.origin});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _OriginBoxState();
+}
+
+class _OriginBoxState extends ConsumerState<OriginBox> {
+  bool isSelected = false;
+  bool hover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final selectedOrigin =
+        ref.watch(settingsProvider).behaviour.autoArrangeOrigin;
+    isSelected = selectedOrigin == widget.origin;
+
+    return Expanded(
+      flex: hover ? 2 : 1,
+      child: MouseRegion(
+        onHover: (event) {
+          if (mounted) {
+            hover = true;
+            setState(() {});
+          }
+        },
+        onExit: (event) {
+          if (mounted) {
+            hover = false;
+            setState(() {});
+          }
+        },
+        child: Button(
+          style: widget.origin == AutoArrangeOrigin.off && isSelected
+              ? ButtonStyle.destructive(
+                  density: ButtonDensity.dense,
+                )
+              : isSelected
+                  ? ButtonStyle.outline(
+                      density: ButtonDensity.dense,
+                    ).withBackgroundColor(
+                      color: theme.colorScheme.primary,
+                      hoverColor: theme.colorScheme.primary)
+                  : ButtonStyle.outline(
+                      density: ButtonDensity.dense,
+                    ).withBackgroundColor(
+                      hoverColor: widget.origin == AutoArrangeOrigin.off
+                          ? theme.colorScheme.destructive
+                          : theme.colorScheme.muted),
+          onPressed: () {
+            ref.read(settingsProvider.notifier).changeAutoArrangeOrigin(
+                  widget.origin,
+                );
+            Db.saveAppSettings(ref.read(settingsProvider));
+          },
+          child: SizedBox(
+            height: 20,
+            child: Center(
+              child: hover
+                  ? OverflowMarquee(
+                      child: Text(
+                        widget.origin.name,
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    )
+                  : Text(
+                      widget.origin.name,
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
