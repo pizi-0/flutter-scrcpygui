6ddb76a0e9380fd22b20c0924f8a2697d85a7c11
pizi-0 <hafizirahim92@gmail.com>
2025-11-18 17:46:00 +0800

    added progress indicator for extraction progress

--- lib/widgets/custom_ui/pg_navigation_rail.dart
+++ lib/widgets/custom_ui/pg_navigation_rail.dart
@@ -7,9 +7,11 @@ import 'package:go_router/go_router.dart';
 import 'package:localization/localization.dart';
 import 'package:scrcpygui/main_screen.dart';
 import 'package:scrcpygui/providers/config_provider.dart';
+import 'package:scrcpygui/providers/missing_icon_provider.dart';
 import 'package:scrcpygui/screens/1.home_tab/home_tab.dart';
 import 'package:scrcpygui/screens/2.connect_tab/connect_tab.dart';
 import 'package:scrcpygui/utils/custom_scheme.dart';
+import 'package:scrcpygui/widgets/extractor_indicator.dart';
 import 'package:scrcpygui/widgets/navigation_shell.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
@@ -67,6 +69,8 @@ class _PgNavigationRailState extends ConsumerState<PgNavigationRail>
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final toExtract = ref.watch(iconsToExtractProvider);
+
     return TapRegion(
       onTapOutside: (event) =>
           ref.read(appSideBarStateProvider.notifier).state = false,
@@ -131,6 +135,8 @@ class _PgNavigationRailState extends ConsumerState<PgNavigationRail>
                 label: el.aboutLoc.title,
                 route: AboutTab.route,
               ),
+              Spacer(),
+              if (toExtract.isNotEmpty) ExtractorIndicator(),
             ],
           ),
         ),
